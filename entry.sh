7f1fc1d4d0840cdea6db7c675b21de7f0e328bd6
Marc Richards <talawahtech@users.noreply.github.com>
2018-03-15 17:13:04 -0500

    Pass in the data directory as an env variable

--- entry.sh
+++ entry.sh
@@ -17,12 +17,12 @@ confd -onetime -backend env -log-level error
 
 
 # Check if DB needs to be initialized
-DB_INITIALIZED_MARKER="/data/core/.db-initialized"
+DB_INITIALIZED_MARKER="${DATA_DIR}/core/.db-initialized"
 
 if [ ! -f ${DB_INITIALIZED_MARKER} ]; then
     echo "initializing core db..."
     stellar-core --conf /stellar-core.cfg -newdb
-    mkdir -p /data/core
+    mkdir -p ${DATA_DIR}/core
     touch ${DB_INITIALIZED_MARKER}
 fi
 
