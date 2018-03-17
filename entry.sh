fa2f1deb4bb9bede574918439b50a0bb2bc9e446
Marc Richards <talawahtech@users.noreply.github.com>
2018-03-17 14:20:02 -0700

    Make DATA_DIR optional and support passing in DB_PORT (optional)

--- entry.sh
+++ entry.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -ue
 
+DATA=${DATA_DIR:-/data}
+PORT=${DB_PORT:-5432}
+
 # Wait for postgres
 export PGPASSWORD=${DB_PASS}
-while ! psql -h "${DB_HOST}" -U "${DB_USER}" -c 'select 1' ${DB_NAME} &> /dev/null ; do
+while ! psql -h "${DB_HOST}" -U "${DB_USER}" -p "${PORT}" -c 'select 1' "${DB_NAME}" &> /dev/null ; do
   echo "Waiting for postgres to be available..."
   sleep 1
 done
@@ -17,12 +20,12 @@ confd -onetime -backend env -log-level error
 
 
 # Check if DB needs to be initialized
-DB_INITIALIZED_MARKER="${DATA_DIR}/core/.db-initialized"
+DB_INITIALIZED_MARKER="${DATA}/core/.db-initialized"
 
 if [ ! -f ${DB_INITIALIZED_MARKER} ]; then
     echo "initializing core db..."
     stellar-core --conf /stellar-core.cfg -newdb
-    mkdir -p ${DATA_DIR}/core
+    mkdir -p ${DATA}/core
     touch ${DB_INITIALIZED_MARKER}
 fi
 
