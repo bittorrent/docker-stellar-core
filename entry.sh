33fef6543809b591257f810d39ce23d1c8d1d695
Marc Richards <talawahtech@users.noreply.github.com>
2018-03-17 21:55:53 -0700

    Use better initialization to avoid overwriting other env vars

--- entry.sh
+++ entry.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 set -ue
 
-DATA=${DATA_DIR:-/data}
-PORT=${DB_PORT:-5432}
+: ${DATA_DIR:=/data}
+: ${DB_PORT:=5432}
 
 # Wait for postgres
 export PGPASSWORD=${DB_PASS}
-while ! psql -h "${DB_HOST}" -U "${DB_USER}" -p "${PORT}" -c 'select 1' "${DB_NAME}" &> /dev/null ; do
+while ! psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -c 'select 1' "${DB_NAME}" &> /dev/null ; do
   echo "Waiting for postgres to be available..."
-  sleep 1
+  sleep 5
 done
 
-# Wait 3 more seconds for postgres to restart itself
-sleep 3
+# Wait 5 more seconds for postgres to restart itself
+sleep 5
 
 
 # Run confd to update config templates with environment variables
@@ -20,12 +20,12 @@ confd -onetime -backend env -log-level error
 
 
 # Check if DB needs to be initialized
-DB_INITIALIZED_MARKER="${DATA}/core/.db-initialized"
+DB_INITIALIZED_MARKER="${DATA_DIR}/core/.db-initialized"
 
 if [ ! -f ${DB_INITIALIZED_MARKER} ]; then
     echo "initializing core db..."
     stellar-core --conf /stellar-core.cfg -newdb
-    mkdir -p ${DATA}/core
+    mkdir -p ${DATA_DIR}/core
     touch ${DB_INITIALIZED_MARKER}
 fi
 
